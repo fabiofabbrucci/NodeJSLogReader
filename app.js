015ff20e2eb8279e826f62192dc9654b3d59f3f6
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2013-04-03 01:27:07 +0200

    JSLint and make test

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
+"use strict";
+
 var port = 8080;
- 
-var app     = require('express')(), 
-    server  = require('http').createServer(app), 
-    io      = require('socket.io').listen(server),
-    fs      = require('fs'),
-    spawn   = require('child_process').spawn;
-    
+
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    fs = require('fs'),
+    spawn = require('child_process').spawn;
+
 var filename = process.argv[2];
 if (!filename) {
     console.log("Usage: node <server.js> <filename>");
-    return false;
+    process.exit(1);
 }
 
 server.listen(port);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  console.log('Client connected');
-  var tail = spawn("tail", ["-f", filename]);
- 
-  tail.stdout.on("data", function (data) {
-    console.log('file changed');
-    socket.emit('tail', data.toString('utf-8'));
-  });
-  
+    console.log('Client connected');
+    var tail = spawn("tail", ["-f", filename]);
+
+    tail.stdout.on("data", function (data) {
+        console.log('file changed');
+        socket.emit('tail', data.toString('utf-8'));
+    });
 });
